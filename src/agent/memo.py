bbb71ba9eb3a3b904796d075d353de2ba72a3ec4
Peter Duan <zddua@student.unimelb.edu.au>
2023-07-28 17:58:59 +1000

    feat : 新增代理转的模式

--- src/agent/memo.py
+++ src/agent/memo.py
@@ -17,34 +17,3 @@ class Memo:
         with open(self.filename, "w") as file:
             json.dump(data, file, indent=2)
 
-
-class StoryBoard:
-    def __init__(self, memo: Memo = None):
-        self.memo = memo
-        self.dialogue_history = self.init_dialogue_history()
-    
-    def init_dialogue_history(self):
-        if self.memo:
-            return self.memo.read_from_file()
-        else:
-            return []
-
-    def add_dialogue(self, role: str, content: str, index=None):
-        if index:
-            self.dialogue_history.insert(index,
-                {"role": role, "content": content})
-        else:
-            self.dialogue_history.append(
-                {"role": role, "content": content},
-            )
-
-    def get_dialogue_history(self,front=None,back=None) -> List[Dict[str, str]]:
-        return self.dialogue_history[front:back]
-
-    def set_dialogue_history(self, dialogue_history: List[Dict[str, str]]):
-        self.dialogue_history = dialogue_history[:]
-    
-    def save(self):
-        if self.memo:
-            self.memo.write_to_file(self.dialogue_history)
-
